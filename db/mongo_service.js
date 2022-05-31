0a23a5aa65374c7974d1a381fc4b0a1e1ea43831
Pauline3414159 <fagie37@hotmail.com>
2022-05-31 16:41:11 -0700

    insert function test of boilerplate

---
+++ db/mongo_service.js
@@ -0,0 +1,23 @@
+const { MongoClient } = require('mongodb');
+const url = 'mongodb://localhost:27017';
+const client = new MongoClient(url);
+const dbName = 'myProject';
+
+async function main() {
+  // Use connect method to connect to the server
+  await client.connect();
+  console.log('Connected successfully to server');
+  const db = client.db(dbName);
+  const collection = db.collection('documents');
+
+  // the following code examples can be pasted here...
+  const insertResult = await collection.insertMany([{ a: 1 }, { a: 2 }, { a: 3 }]);
+  console.log('Inserted documents =>', insertResult);
+
+  return 'done.';
+}
+
+main()
+  .then(console.log)
+  .catch(console.error)
+  .finally(() => client.close());
