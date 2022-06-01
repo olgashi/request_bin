c410b89fa29d29e0e46dc9a605376d2ac6297ead
Pauline3414159 <fagie37@hotmail.com>
2022-06-01 04:19:40 -0700

    added try catch, working

--- db/mongo_service.js
+++ db/mongo_service.js
@@ -1,42 +1,37 @@
 const { MongoClient } = require('mongodb');
 const url = 'mongodb://localhost:27017';
 const client = new MongoClient(url);
-const dbName = 'myProject';
 
-async function main() {
-  // Use connect method to connect to the server
-  await client.connect();
-  console.log('Connected successfully to server');
-  const db = client.db(dbName);
-  const collection = db.collection('documents');
-
-  // the following code examples can be pasted here...
-  const insertResult = await collection.insertMany([{ a: 1 }, { a: 2 }, { a: 3 }]);
-  console.log('Inserted documents =>', insertResult);
-
-  return 'done.';
-}
-
+// request is a regular javascript object. Returns a promise that resolves to a javascript object like { acknowledged: true, insertedId: 3 } or an error
 async function addRequest(request, reqId, dbName = "requestBin") {
-  await client.connect();
-  const db = client.db(dbName);
-  const collection = db.collection('requests');
-  request._id = reqId
-  const inserted = await collection.insertOne(request);
-  client.close()
-  return inserted
+  try {
+    await client.connect();
+    const db = client.db(dbName);
+    const collection = db.collection('requests');
+    request._id = reqId
+    const inserted = await collection.insertOne(request);
+    await client.close()
+    return inserted
+  } catch (error) {
+    return error
+  }
+  
 }
-
+// returns a promise that resolves to JSON request object or null if not found
 async function findRequest(reqId, dbName = "requestBin") {
-  await client.connect()
-  const db = client.db(dbName);
-  const collection = db.collection('requests');
-  const found = await collection.findOne({_id: reqId})
-  client.close()
-  return found
+  try {
+    await client.connect()
+    const db = client.db(dbName);
+    const collection = db.collection('requests');
+    const found = await collection.findOne({_id: reqId})
+    await client.close()
+    return JSON.stringify(found)
+  } catch (error) {
+    return error
+  } 
 }
 
-// main()
-//   .then(console.log)
-//   .catch(console.error)
-//   .finally(() => client.close());
+module.exports = {
+  addRequest,
+  findRequest,
+}
