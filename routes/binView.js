6291f309c91825927fa28ee5349e98b5d5149770
Anne Jones <anne1365@outlook.com>
2022-06-01 19:10:30 -0700

    Updated routes to interface with db & include unique ID generation

--- routes/binView.js
+++ routes/binView.js
@@ -1,18 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const { findRequest } = require('lib\db-mongo\mongo_service.js');
+const { getRequestsByBinID } = require('lib\db-pg\requests-query.js');
 
 /* GET bin view */
 router.get('/:binID', function(req, res, next) {
-  console.log('bin view')
-  // retrieve data for binID from postgres db
-  // render bin view template
+  let binID = req.params.binID;
+  getRequestsByBinID(binID); // retrieve bin data from postgres
+  // call to front end with bin data
   res.send('200 OK');
 });
 
 /* GET request view */
 router.get('/:binID/:requestID', function (req, res, next) {
-  // retrieve data for binID & binID+requestID from postgres db
-  // render event view template
+  let binID = req.params.binID;
+  let reqID = req.params.requestID;
+  getRequestsByBinID(binID); // retrieve bin data from postgres
+  findRequest(reqID);        // retrieve request data from mongoDB
+  // call to front end with bin & request data
   res.send('200 OK');
 });
 
--- routes/endpoint.js
+++ routes/endpoint.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const { all } = require('.');
 const router = express.Router();
+const { guid } = require('../lib/utils/guid');
+const { addRequest } = require('lib\db-mongo\mongo_service.js');
+const { createRequest } = require('lib\db-pg\requests-query.js'); // Sig: createRequest(reqId, binId, method, reqPath)
 
 /* Submit HTTP request to endpoint */
 router.route('/*')
   .all(function(req, res, next) {
-    // parse out binID
-    // generate request ID
-    // send raw HTTP request data to MongoDB here
+    // parse binID from URL
+    let reqID = guid(27);
+    addRequest(req, reqID); // add request data to MongoDB
+    createRequest(reqID, binID, req.method, req.path); // add request data to postgres
     res.send({"success": true});
   })
 
