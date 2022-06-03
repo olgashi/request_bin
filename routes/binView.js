9c4177793b80aeee103344350756d621f0ed17e0
Pauline3414159 <fagie37@hotmail.com>
2022-06-02 17:11:54 -0700

    all routes working

--- routes/binView.js
+++ routes/binView.js
@@ -1,22 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const { findRequest } = require('../lib/db-mongo/mongo_service.js');
-const { getRequestsByBinID } = require('../lib/db-pg/requests-query.js');
+const { getRequestsByBinId } = require('../lib/db-pg/requests-query.js');
 
 /* GET bin view */
 router.get('/:binID', function(req, res, next) {
   let binID = req.params.binID;
-  getRequestsByBinID(binID); // retrieve bin data from postgres
+  getRequestsByBinId(binID); // retrieve bin data from postgres
   // call to front end with bin data
   res.send('200 OK');
 });
 
 /* GET request view */
-router.get('/:binID/:requestID', function (req, res, next) {
+router.get('/:binID/:requestID', async function (req, res, next) {
   let binID = req.params.binID;
   let reqID = req.params.requestID;
-  getRequestsByBinID(binID); // retrieve bin data from postgres
-  findRequest(reqID);        // retrieve request data from mongoDB
+  let preview = await getRequestsByBinId(binID); // retrieve bin data from postgres
+  let full = await findRequest(reqID);        // retrieve request data from mongoDB
   // call to front end with bin & request data
   res.send('200 OK');
 });
